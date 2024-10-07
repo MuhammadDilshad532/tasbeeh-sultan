193c7248a5394036a71b87fa28d2300e89c161e8
Dilshad <dilshadahamad532280@gmail.com>
2024-10-07 18:40:56 +0500

    add increment button and change title name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
 function App() {
-
-
+  const [count , setCount]= useState(0)
   return (
     <>
-      
-      <h1>Count is </h1>
-     
+      <div className="text-center mt-40 text-2xl">
+        <h1 className="font-bold">Count is {count} </h1>
+        <button onClick={()=> setCount((count) => count +1)}>increment</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
