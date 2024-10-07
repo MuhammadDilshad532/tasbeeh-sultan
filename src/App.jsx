01ba543f77ed23e215fc1088a3d17cd082eabd9c
Dilshad <dilshadahamad532280@gmail.com>
2024-10-07 20:55:58 +0500

    add decrement and reset button

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ function App() {
   const [count , setCount]= useState(0)
   return (
     <>
-      <div className="text-center mt-40 text-2xl">
+      <div className="text-center mt-40 text-2xl ">
         <h1 className="font-bold">Count is {count} </h1>
-        <button onClick={()=> setCount((count) => count +1)}>increment</button>
+        <button onClick={()=> setCount((count) => count +1)} className="bg-slate-400 p-1 rounded-lg">Increment</button>
+        <button onClick={()=> setCount((count)=> count -1)} className="ml-3 bg-slate-400 p-1 rounded-lg">Decrement</button>
+        <button onClick={()=> setCount((count)=> 0)}  className="ml-3 bg-slate-400 p-1 rounded-lg">Reset</button>
       </div>
     </>
   );
