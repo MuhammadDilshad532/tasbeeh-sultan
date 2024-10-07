49aaa00ea34c8563b006ce8ef3b0be0e3b0eedc9
Dilshad <dilshadahamad532280@gmail.com>
2024-10-08 00:27:47 +0500

    add disabled on zero

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="text-center mt-40 text-2xl ">
         <h1 className="font-bold">Count is {count} </h1>
         <button onClick={()=> setCount((count) => count +1)} className="bg-slate-400 p-1 rounded-lg">Increment</button>
-        <button onClick={()=> setCount((count)=> count -1)} className="ml-3 bg-slate-400 p-1 rounded-lg">Decrement</button>
+        <button disabled={count < 1} onClick={()=> setCount((count)=> count -1)} className="ml-3 bg-slate-400 p-1 rounded-lg">Decrement</button>
         <button onClick={()=> setCount((count)=> 0)}  className="ml-3 bg-slate-400 p-1 rounded-lg">Reset</button>
       </div>
     </>
